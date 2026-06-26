c5b382131dcc88be9be240f24f77547ae4370673
thoriqfathu <syafikasif@gmail.com>
2026-06-26 08:40:02 +0700

    debugging test

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "<pre>";
-print_r(get_loaded_extensions());
-echo "</pre>";
+echo "<h2>PHP Extension Check</h2>";
+
+echo "PDO: " . (extension_loaded('pdo') ? 'ADA' : 'TIDAK ADA') . "<br>";
+echo "pdo_mysql: " . (extension_loaded('pdo_mysql') ? 'ADA' : 'TIDAK ADA') . "<br>";
+echo "mysqli: " . (extension_loaded('mysqli') ? 'ADA' : 'TIDAK ADA') . "<br>";
